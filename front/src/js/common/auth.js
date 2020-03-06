1daecacce71d84571f984f874e299bc7ce5922bf
chenyin-hong <1763324734@qq.com>
2020-03-06 19:06:36 +0800

    登录相关代码更新

---
+++ front/src/js/common/auth.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".singinanniu").click(function () {
+        $(".waraper").show()
+    });
+
+    $(".close-btn").click(function () {
+        $(".waraper").hash()
+    })
+});
