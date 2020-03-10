8f1427bb76393e621fb4c4318b3dbcbe2284ded6
chenyin-hong <1763324734@qq.com>
2020-03-10 19:22:32 +0800

    登录js代码模态对话框更新

--- front/src/js/common/auth.js
+++ front/src/js/common/auth.js
@@ -1,9 +1,24 @@
 $(function () {
     $(".singinanniu").click(function () {
-        $(".waraper").show()
+        $(".mask_wrapper").show();
     });
 
     $(".close-btn").click(function () {
-        $(".waraper").hash()
+        $(".mask_wrapper").hide();
     })
 });
+
+$(function () {
+    $(".qiehuan").click(function () {
+        var yangshi=$(".up_in_wrapper").css("marginLeft");
+        var yangshizuhanhuan = parseInt(yangshi);
+        console.log(yangshi);
+        if (yangshizuhanhuan<0) {
+            $(".up_in_wrapper").animate({marginLeft:"0px"});
+        }
+        else {
+            $(".up_in_wrapper").animate({marginLeft:"-400px"});
+
+        }
+    })
+})
